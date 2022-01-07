408f59d212406f9e1bad2797f4e6d6eace958171
TBallmeyer <97289527+TBallmeyer@users.noreply.github.com>
2022-01-07 13:01:25 +0100

    Create html.h

---
+++ html.h
@@ -0,0 +1,10 @@
+String HTMLIndex =
+  "<html>\n"
+  "<head>\n"
+  "<title>RGB-LED</title></head>"
+  "<body>\n"
+  "<h1>Webserver</h1>"
+  "<form action='http://192.168.178.54/'>"       //ip adresse festlegen
+  "</form>"
+  "</body>"
+  "</html>";
